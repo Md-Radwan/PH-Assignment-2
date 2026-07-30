3e517f686a318473df05b2a737c4468249efe194
Md-Radwan <redwanhosen4@gmail.com>
2026-07-30 10:14:38 +0600

    Problem 1 is added

---
+++ Problem-1.js
@@ -0,0 +1,16 @@
+function matchWinner(teamAGoals, teamBGoals) {
+  if (typeof teamAGoals !== "number" || typeof teamBGoals !== "number") {
+    return "Invalid";
+  } else if (teamAGoals > teamBGoals) {
+    return "Team A Won";
+  } else if (teamAGoals < teamBGoals) {
+    return "Team A Won";
+  } else {
+    return "Draw";
+  }
+}
+
+console.log(matchWinner(2, 1));
+console.log(matchWinner(1, 3));
+console.log(matchWinner(2, 2));
+console.log(matchWinner("3", 2));
