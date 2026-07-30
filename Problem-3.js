b83f903c98fcd418885a44f55f345d0b7bf7a85a
Md-Radwan <redwanhosen4@gmail.com>
2026-07-30 14:14:43 +0600

    Problem 3 is added

---
+++ Problem-3.js
@@ -0,0 +1,16 @@
+function calculateAiCost(tokensUsed) {
+  let cost = tokensUsed - 500;
+  let totalCost = cost / 100;
+
+  if (typeof tokensUsed !== "number" || tokensUsed < 0) {
+    return "Invalid";
+  } else if (tokensUsed <= 500) {
+    return 0;
+  } else return Math.floor(totalCost) * 5;
+}
+
+console.log(calculateAiCost(300));
+console.log(calculateAiCost(500));
+console.log(calculateAiCost(650));
+console.log(calculateAiCost(-10));
+console.log(calculateAiCost("500"));
